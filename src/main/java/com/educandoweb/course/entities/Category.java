9968a05d5dc67292e28cfe3289d6bbc5f287d81b
Luiz Felipe Isobata Yoshiassu <luiz.isobata@gmail.com>
2024-01-23 10:18:22 -0300

    OrderItem, many-to-many association with extra attributes - PART 1

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -73,6 +73,4 @@ public class Category  implements Serializable {
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-
-
 }
--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -93,5 +93,4 @@ public class Order implements Serializable {
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
 	}
-
 }
